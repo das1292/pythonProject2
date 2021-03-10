67ee01447ca76abdc476274a2d5a44a73a94e41d
das1292 <jakub.klos.10@wp.pl>
2021-03-10 19:06:08 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Zadanie 1
+# sport = ["piłka nożna", "siatkówka", "koszykówka"]
+# print(sport)
+# sport.reverse()
+# print(sport)
+# sportm = ("piłka ręczna", "kolarstwo", "żużel")
+# sport.append(sportm)
+# print(sport)
+
+#Zadanie2
